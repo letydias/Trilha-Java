014a6cc09c71d911e9c49165e5a63db381e3d2ab
LeticiaD <lelenassioriginal@gmail.com>
2024-05-14 21:42:02 -0300

    Estruturas Condicionais 2

---
+++ Estruturas Condicionais em Java/src/ResultadoEscolar.java
@@ -0,0 +1,27 @@
+public class ResultadoEscolar {
+/* Condicional encadeada:
+
+
+        public static void main(String[] args) {
+            int nota = 6;
+            if (nota >= 7)
+                System.out.println("Aprovado");
+            else if (nota >= 5 && nota < 7)  //Condicional Encadeada
+                System.out.println("Recuperação");
+            else
+                System.out.println("Reprovado");
+        }
+    }
+    */
+/* Condicional Ternária */
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
+
+
+
